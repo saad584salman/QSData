63b8fb5e3ed580576d40276d58dbebcf54bf527e
Saad Salman <saad584salman@gmail.com>
2025-05-04 02:14:28 +0500

    add boilerplate express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Docker!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
